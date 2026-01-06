ff83140cbff4f237ca87470dcb03f239bc3d1c35
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2026-01-06 19:23:51 +0530

    Create 2235-Add-Two-Integers.java

---
+++ Easy/2235-Add-Two-Integers.java
@@ -0,0 +1,11 @@
+// LeetCode 0283 - Add Two Integers
+// Solved on: 6 January 2026 
+// Approach: Direct arithmetic addition
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
