36873d2ce6cd6e1b8002557969c5cd8788780f3c
Arnold Joel Joshua <marnoldjoeljoshua@gmail.com>
2026-01-06 07:25:22 +0530

    Create 0283-Move-Zeros.java

---
+++ Easy/0283-Move-Zeros.java
@@ -0,0 +1,17 @@
+// LeetCode 0283 - Move Zeros
+// Solved on: 6 January 2026 
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        for(int j=0; j<nums.length; j++){
+            if(nums[j] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+        Arrays.toString(nums);
+    }
+}
